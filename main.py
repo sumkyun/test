e3c641942d89309e781d0bd425cba513ee7df824
sumkyun <70997770+sumkyun@users.noreply.github.com>
2023-09-14 10:36:56 +0900

    구구단까지 만들었음

---
+++ main.py
@@ -0,0 +1,6 @@
+from main_func import *
+
+say_Hello()
+for i in range(2, 10):
+    say_GugudanStart(i)
+    say_Gugudan(i)
---
+++ main_func.py
@@ -0,0 +1,9 @@
+def say_Hello():
+    print("안녕 구구단 출력해줄게")
+
+def say_GugudanStart(i):
+    print(i, "단을 출력할게")
+
+def say_Gugudan(i):
+    for j in range(1, 10):
+        print(i, "X", j, "=", i*j)
